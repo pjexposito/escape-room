32d0bde2620cac6825ab341117af65657380c26c
pjexposito <pjexposito@gmail.com>
2022-08-23 14:57:14 +0200

    Actualizar main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import music
-
 def on_button_pressed_a():
     global Suma_1
     if Suma_1 == 0:
@@ -7,11 +5,57 @@ def on_button_pressed_a():
     elif Suma_1 == 1:
         Suma_1 = 2
     elif Suma_1 == 3:
-        music.play_melody("C5 B A G F E D C ", 120)
-        basic.show_string("Abre la nevera")
+        music.play_melody("G C C D D E F G ", 500)
+        Suma_1 = 4
+        basic.show_string("Conejo Elena")
     else:
         Suma_1 = 0
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    if Suma_1 == 3:
+        basic.show_icon(IconNames.EIGTH_NOTE)
+        music.begin_melody(["g4:2",
+                "c5",
+                "c",
+                "d",
+                "d",
+                "e:4",
+                "f:2",
+                "g",
+                "f",
+                "e",
+                "e",
+                "d",
+                "d",
+                "c",
+                "g",
+                "f",
+                "e",
+                "e",
+                "e",
+                "f",
+                "e",
+                "d",
+                "d",
+                "g",
+                "f",
+                "e",
+                "e",
+                "e",
+                "f",
+                "e",
+                "d",
+                "d",
+                "d",
+                "g",
+                "e",
+                "c",
+                "d",
+                "d",
+                "c"],
+            MelodyOptions.ONCE)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
     global Suma_1
@@ -23,6 +67,7 @@ input.on_button_pressed(Button.B, on_button_pressed_b)
 
 Suma_1 = 0
 Suma_1 = 0
+Melodia = "[f f]"
 
 def on_forever():
     if Suma_1 == 0:
--- main.ts
+++ main.ts
@@ -4,10 +4,17 @@ input.onButtonPressed(Button.A, function () {
     } else if (Suma_1 == 1) {
         Suma_1 = 2
     } else if (Suma_1 == 3) {
-        music.playMelody("C5 B A G F E D C ", 120)
-        basic.showString("Abre la nevera")
+        music.playMelody("G C C G D E F G ", 411)
+        Suma_1 = 4
+        basic.showString("Conejo Elena")
     } else {
         Suma_1 = 0
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (Suma_1 == 3) {
+        basic.showIcon(IconNames.EigthNote)
+        music.beginMelody(["g4:2", "c5", "c", "d", "d", "e:4", "f:2", "g", "f", "e", "e", "d", "d", "c", "g", "f", "e", "e", "e", "f", "e", "d", "d", "g", "f", "e", "e", "e", "f", "e", "d", "d", "d", "g", "e", "c", "d", "d", "c"], MelodyOptions.Once)
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -19,6 +26,7 @@ input.onButtonPressed(Button.B, function () {
 })
 let Suma_1 = 0
 Suma_1 = 0
+let Melodia = "[f f]"
 basic.forever(function () {
     if (Suma_1 == 0) {
         basic.clearScreen()
